67b362ea3652b3e3ee6db30ae2786c5d0c9a9426
Mark <zakharov.mark22@gmail.com>
2017-11-09 02:17:29 +0300

    added css and js

--- action.php
+++ action.php
@@ -1,2 +1,12 @@
-Hello, <?php echo htmlspecialchars($_POST['name']); ?>.
-You are <?php echo (int)$_POST['age']; ?> years old.
+<!DOCTYPE HTML>
+
+<head>
+<meta charset="utf-8">
+<link href="./style.css" rel="stylesheet">
+<title> My website </title>
+</head>
+
+<body>
+	<h1>Hello, <strong> <?php echo htmlspecialchars($_POST['name']); ?> </strong>.<h1>
+	<h2>You are <?php echo (int)$_POST['age']; ?> years old.<h2>
+</body>
---
+++ alert.js
@@ -0,0 +1,4 @@
+var userAge = confirm("Are you 18 years old?");
+if (userAge != true){
+	alert("It's okay. My site doesn't contain adult content");
+}
