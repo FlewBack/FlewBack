c244c5b4f983d949be42b2282c0a2191a5a03ea7
Mark <zakharov.mark22@gmail.com>
2017-12-16 10:04:14 +0300

    sorted

---
+++ test-01/action.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+
+<head>
+<meta charset="utf-8">
+<link href="./style.css" rel="stylesheet">
+<title> My website </title>
+</head>
+
+<body>
+	<h1>Hello, <strong> <?php echo htmlspecialchars($_POST['name']); ?> </strong>.<h1>
+	<h2>You are <?php echo (int)$_POST['age']; ?> years old.<h2>
+	<h3> Well. <h3>
+</body>
